3b49f5e5f39ccde589240128b03cf71c78a5fc35
Wojtek <4golda4@gmail.com>
2020-06-01 23:44:27 +0200

    funkcja zliczajaca za pomocą domknięć

--- dzien 10 licznik czasu spedzonego na stronie/script.js
+++ dzien 10 licznik czasu spedzonego na stronie/script.js
@@ -1,6 +1,12 @@
 let number = 0;
 window.addEventListener("click", () => {
     number++;
-    console.log(number);
+    document.body.textContent = number;
     // tradycyjnie
 })
+
+// Closure (domknięcie) to mechanizm, który pomaga nam uzyskać dostęp do danej wartości poza funkcją i gdy ta funkcja nie jest już aktywna.
+
+// Closure - zmienna istnieje w innej funkcji, mimo, że funkcja, w której była stworzona ta zmienna nie jest już aktywna. 
+
+//W pamieci programu zmienna istnieje jeśli prowadzi do niej referencja z innej funkcji. Mozemy mieć do niej dostęp, ale także je zmieniać. 
