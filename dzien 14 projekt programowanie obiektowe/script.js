27d438ea7e44afa78511d4cd45806d3a55b181ce
Wojtek <4golda4@gmail.com>
2020-06-27 23:42:34 +0200

    zabezpieczenie przed wpisaniem wartosci mniejszej niz 0 w input

---
+++ dzien 14 projekt programowanie obiektowe/script.js
@@ -0,0 +1,8 @@
+var input = document.querySelector("input");
+input.addEventListener("change", function () {
+    if (input.value < 0) {
+        alert("nie mozesz wpisac wartosci ujemnej");
+        input.value = 0;
+    }
+
+})
